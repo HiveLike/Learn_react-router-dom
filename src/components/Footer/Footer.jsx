9cf6d27ee3c491bef05fe0c7a9870f8ae427e2cf
Нагимуллин Ильшат Рифатович <251911@mck-ktits.ru>
2023-01-21 16:38:16 +0300

    Learn_react-router-dom

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+    return (
+        <footer>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">
+                            Главная
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/about">
+                            О нас
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer;
